5e34fa7e2e861b9b934af1ec27c99093ce9723b4
smorgenfeld <smorgenfeld@users.noreply.github.com>
2017-02-15 13:46:54 -0800

    prep 2/15/17

--- resources/tank.js
+++ resources/tank.js
@@ -1,3 +1,64 @@
 function gettank() {
-  
+  tank = {
+    width: 20,
+    height:20,
+    x: 0,
+    y: 0,
+    rx: wwidth/2,
+    ry: wheight/2,
+    btheta: 0,
+    ttheta: 0,
+    dir: {
+      f: false,
+      b: false,
+      r: false,
+      l: false,
+      reset: function() {
+        this.dir.f = false;
+        this.dir.b = false;
+        this.dir.r = false;
+        this.dir.l = false;
+      }
+    },
+    v: {
+      x: 0,
+      y: 0,
+      max: 5,
+    },
+    update: function() {
+      var ANG = 0.707107;
+      this.v.y = 0;
+      this.v.x = 0;
+      if (this.dir.f && this.dir.l) {
+        this.v.x = this.v.max * -ANG;
+        this.v.y = this.v.max * ANG;
+      }
+      else if (this.dir.f && this.dir.r) {
+        this.v.x = this.v.max * ANG;
+        this.v.y = this.v.max * ANG;
+      }
+      else if (this.dir.b && this.dir.l) {
+        this.v.x = this.v.max * -ANG;
+        this.v.y = this.v.max * -ANG;
+      }
+      else if (this.dir.b && this.dir.r) {
+        this.v.x = this.v.max * ANG;
+        this.v.y = this.v.max * -ANG;
+      }
+      else if (this.dir.b) {
+        this.v.y = -this.v.max;
+      }
+      else if (this.dir.f) {
+        this.v.y = this.v.max;
+      }
+      else if (this.dir.l) {
+        this.v.x = -this.v.max;
+      }
+      else if (this.dir.r) {
+        this.v.x = this.v.max;
+      }
+    },
+  }
+  return tank;
 }
+
